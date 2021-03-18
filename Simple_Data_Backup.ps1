aa407b7fe9f38618230850741410e68b17e24122
Curar <58891522+Curar@users.noreply.github.com>
2021-03-18 20:21:55 +0100

    Komentarz
    Zmiana wielkości liter kodu na jednolite .

--- Simple_Data_Backup.ps1
+++ Simple_Data_Backup.ps1
@@ -1,2 +1,2 @@
-Get-ChildItem -Path "C:\BACKUP_FOLDERS\" -Directory -recurse | where {$_.LastWriteTime -le $(Get-date).Adddays(-14)} | Remove-Item -recurse -force
-Get-childitem -Path "D:\WindowsImageBackup\" | where-object {$_.LastWriteTime -le (Get-date).AddDays(-7)} | Copy-Item -destination "C:\BACKUP_FOLDERS\" -Recurse -Force
+Get-ChildItem -Path "C:\BACKUP_FOLDERS\" -Directory -recurse | where-object {$_.LastWriteTime -le $(Get-date).Adddays(-14)} | Remove-Item -Recurse -Force
+Get-ChildItem -Path "D:\WindowsImageBackup\" | where-object {$_.LastWriteTime -le (Get-date).AddDays(-7)} | Copy-Item -destination "C:\BACKUP_FOLDERS\" -Recurse -Force
